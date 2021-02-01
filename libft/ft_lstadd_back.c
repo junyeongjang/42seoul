9fda1d94f503f77243e7c8c5a2b8da1e885a5410
junyeongjang <junyeong.dev@gmail.com>
2021-02-01 22:25:28 +0900

    bonus

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjang <jjang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/01 16:01:43 by jjang             #+#    #+#             */
+/*   Updated: 2021/02/01 16:25:29 by jjang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last;
+	
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	new->next = last->next;
+	last->next = new;
+}
