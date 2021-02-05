af23752be9f12ef1c801013eec10be2ada98a9d2
junyeongjang <junyeong.dev@gmail.com>
2021-02-05 15:38:23 +0900

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jjang <jjang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 13:26:34 by jjang             #+#    #+#             */
-/*   Updated: 2021/02/03 13:58:53 by jjang            ###   ########.fr       */
+/*   Updated: 2021/02/05 15:36:47 by jjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t		i;
-	size_t		j;
-	size_t		dest_len;
-	size_t		src_len;
+	size_t	i;
+	size_t	dest_len;
+	size_t	src_len;
 
-	j = 0;
-	while (j < size && dest[j])
-		j++;
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
 	i = 0;
-	while (i < src_len && dest_len + i + 1 < size)
+	if (size < dest_len + 1)
+		return (size + src_len);
+	if (size > dest_len + 1)
 	{
-		dest[dest_len + i] = src[i];
-		i++;
+		while (src[i] != '\0' && dest_len + 1 + i < size)
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
 	}
 	dest[dest_len + i] = '\0';
-	return (j + src_len);
+	return (dest_len + src_len);{
 }
